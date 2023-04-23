2d017776421f4a5f57ba815a12404219ecbeab10
TizCloud <45397970+TizCloud@users.noreply.github.com>
2023-04-23 23:40:09 +0700

    Add files via upload

---
+++ 04-01-CallModule.py
@@ -0,0 +1,8 @@
+# Demo 04-01
+import CalculateAreaRectangle
+
+print("Area of Rectangle : ",CalculateAreaRectangle.rectangle(10, 15))
+print("Area of Squre : ",CalculateAreaRectangle.squre(5, 5))
+print("Area of Parallelogram : ",CalculateAreaRectangle.parallelogram(5.5, 10.5))
+print("Area of Trapezoid :",CalculateAreaRectangle.trapezoid(5, 2.5))
+
---
+++ 04-02-CallModulePackage.py
@@ -0,0 +1,10 @@
+# Demo 04-02
+import CalculateArea
+
+print("Area of squre : ",CalculateArea.squre(5.5, 5.5))
+print("Area of rectangle : ",CalculateArea.rectangle(5.5, 7.5))
+print("Area of triangle : ",CalculateArea.triangle(3.5, 2.5))
+print("Area of equilateral : ",CalculateArea.equilateral(2.5))
+
+
+
